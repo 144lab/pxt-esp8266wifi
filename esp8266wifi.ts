6e83b996c1fbe4b3365b0edf3af128f22c654377
Irieda Noboru <irieda@144lab.com>
2018-11-15 16:42:16 +0900

    fix recv memory overflow

--- esp8266wifi.ts
+++ esp8266wifi.ts
@@ -13,98 +13,101 @@ namespace esp8266wifi {
   let lastError: string;
 
   function parse(t: string) {
-    let args: string[] = [];
-    let arg: string = "";
-    for (let i = 0; i < t.length; i++) {
-      let c = t.charCodeAt(i);
-      if (c < 0x20 || c > 0x7e) {
-        continue;
+      let args: string[] = [];
+      let arg: string = "";
+      for (let i = 0; i < t.length; i++) {
+          let c = t.charCodeAt(i);
+          if (c < 0x20 || c > 0x7e) {
+              continue;
+          }
+          if (t[i] != ",") {
+              arg += t[i];
+          } else {
+              args.push(arg);
+              arg = "";
+          }
       }
-      if (t[i] != ",") {
-        arg += t[i];
-      } else {
-        args.push(arg);
-        arg = "";
-      }
-    }
-    args.push(arg);
-    return args;
+      args.push(arg);
+      return args;
   }
 
   function bytes2str(array: number[]): string {
-    let line: string = "";
-    for (let i = 0; i < array.length; i++) {
-      let v = array[i];
-      if (v == 0) {
-        return line;
+      let line: string = "";
+      for (let i = 0; i < array.length; i++) {
+          let v = array[i];
+          if (v == 0) {
+              return line;
+          }
+          line += String.fromCharCode(v);
       }
-      line += String.fromCharCode(v);
-    }
-    return line;
+      return line;
   }
 
   function writeData(chunk: string) {
-    pins.digitalWritePin(DigitalPin.P16, 0);
-    pins.spiWrite(2);
-    pins.spiWrite(0);
-    for (let i: number = 0; i < 32; i++) {
-      if (i < chunk.length) {
-        pins.spiWrite(chunk.charCodeAt(i));
-      } else {
-        pins.spiWrite(0);
+      pins.digitalWritePin(DigitalPin.P16, 0);
+      pins.spiWrite(2);
+      pins.spiWrite(0);
+      for (let i: number = 0; i < 32; i++) {
+          if (i < chunk.length) {
+              pins.spiWrite(chunk.charCodeAt(i));
+          } else {
+              pins.spiWrite(0);
+          }
       }
-    }
-    pins.digitalWritePin(DigitalPin.P16, 1);
+      pins.digitalWritePin(DigitalPin.P16, 1);
   }
 
   function readData(): number[] {
-    let res: number[] = [];
-    pins.digitalWritePin(DigitalPin.P16, 0);
-    pins.spiWrite(3);
-    pins.spiWrite(0);
-    for (let i: number = 0; i < 32; i++) {
-      res.push(pins.spiWrite(0));
-    }
-    pins.digitalWritePin(DigitalPin.P16, 1);
-    return res;
+      let res: number[] = [];
+      pins.digitalWritePin(DigitalPin.P16, 0);
+      pins.spiWrite(3);
+      pins.spiWrite(0);
+      for (let i: number = 0; i < 32; i++) {
+          res.push(pins.spiWrite(0));
+      }
+      pins.digitalWritePin(DigitalPin.P16, 1);
+      return res;
   }
 
   function send(content: string) {
-    for (let i: number = 0; i < content.length; i += 32) {
-      writeData(content.substr(i, 32));
-    }
-    if (content.length % 32 == 0) {
-      writeData("\x00");
-    }
+      for (let i: number = 0; i < content.length; i += 32) {
+          writeData(content.substr(i, 32));
+      }
+      if (content.length % 32 == 0) {
+          writeData("\x00");
+      }
   }
 
-  function _recv(): string {
-    let res = "";
-    let running = true;
-    while (running) {
-      let chunk = bytes2str(readData());
-      if (chunk.length < 32) {
-        running = false;
+  function _recv(max: number = 2048): string {
+      let res = "";
+      let running = true;
+      while (running) {
+          let chunk = bytes2str(readData());
+          if (chunk.length < 32) {
+              running = false;
+          }
+          res += chunk;
+          if (res.length > max) {
+              return ""
+          }
       }
-      res += chunk;
-    }
-    return res;
+      return res;
   }
 
-  function recv(timeout: number = 500): string {
-    let begin = input.runningTime();
-    while (true) {
-      let now = input.runningTime();
-      if (now - begin > timeout) {
-        // recv timeout
-        return "";
+  function recv(max: number = 2048, timeout: number = 500): string {
+      let begin = input.runningTime();
+      while (true) {
+          let now = input.runningTime();
+          if (now - begin > timeout) {
+              // recv timeout
+              return "";
+          }
+          let res = _recv(max);
+          if (res.length > 0) {
+              return res;
+          }
+          basic.pause(20);
       }
-      let res = _recv();
-      if (res.length > 0) {
-        return res;
-      }
-      basic.pause(20);
-    }
   }
 
   /**
@@ -112,7 +115,7 @@ namespace esp8266wifi {
    */
   //% blockId=esp8266wifi_on_connect_succeeded block="on connect succeeded"
   export function onConnectSucceeded(cb: Action): void {
-    control.onEvent(event_src_esp8266wifi, event_connect_succeeded, cb);
+      control.onEvent(event_src_esp8266wifi, event_connect_succeeded, cb);
   }
 
   /**
@@ -120,7 +123,7 @@ namespace esp8266wifi {
    */
   //% blockId=esp8266wifi_on_connect_failed block="on connect failed"
   export function onConnectFailed(cb: Action): void {
-    control.onEvent(event_src_esp8266wifi, event_connect_failed, cb);
+      control.onEvent(event_src_esp8266wifi, event_connect_failed, cb);
   }
 
   /**
@@ -128,7 +131,7 @@ namespace esp8266wifi {
    */
   //% blockId=esp8266wifi_on_response_succeeded block="on response succeeded"
   export function onResponseSucceeded(cb: Action): void {
-    control.onEvent(event_src_esp8266wifi, event_response_succeeded, cb);
+      control.onEvent(event_src_esp8266wifi, event_response_succeeded, cb);
   }
 
   /**
@@ -136,7 +139,7 @@ namespace esp8266wifi {
    */
   //% blockId=esp8266wifi_on_response_failed block="on response failed"
   export function onResponseFailed(cb: Action): void {
-    control.onEvent(event_src_esp8266wifi, event_response_failed, cb);
+      control.onEvent(event_src_esp8266wifi, event_response_failed, cb);
   }
 
   /**
@@ -148,58 +151,58 @@ namespace esp8266wifi {
   //% block="connect Wi-Fi ssid: %ssid| password: %password"
   //% blockExternalInputs = 1
   export function connect(ssid: string, password: string): void {
-    if (!initialized) {
-      pins.digitalWritePin(DigitalPin.P12, 0);
-      basic.pause(100);
-      pins.digitalWritePin(DigitalPin.P12, 1);
-      basic.pause(200);
-      pins.digitalWritePin(DigitalPin.P16, 1);
-      pins.spiPins(DigitalPin.P15, DigitalPin.P14, DigitalPin.P13);
-      pins.spiFormat(8, 3);
-      pins.spiFrequency(1000000);
-      recv(100);
-      initialized = true;
-    }
-    send("c," + ssid + "," + password);
-    let args = parse(recv(10000));
-    if (args.length > 3) {
-      lastResult = args[3];
-      if (args[1] === "200") {
-        control.raiseEvent(event_src_esp8266wifi, event_connect_succeeded);
-        return;
+      if (!initialized) {
+          pins.digitalWritePin(DigitalPin.P12, 0);
+          basic.pause(100);
+          pins.digitalWritePin(DigitalPin.P12, 1);
+          basic.pause(200);
+          pins.digitalWritePin(DigitalPin.P16, 1);
+          pins.spiPins(DigitalPin.P15, DigitalPin.P14, DigitalPin.P13);
+          pins.spiFormat(8, 3);
+          pins.spiFrequency(1000000);
+          readData();
+          initialized = true;
       }
-    }
-    if (args.length > 2) {
-      lastError = args[2];
-    } else {
-      lastError = "invalid response";
-    }
-    control.raiseEvent(event_src_esp8266wifi, event_connect_failed);
+      send("c," + ssid + "," + password);
+      let args = parse(recv(128, 10000));
+      if (args.length > 3) {
+          lastResult = args[3];
+          if (args[1] === "200") {
+              control.raiseEvent(event_src_esp8266wifi, event_connect_succeeded);
+              return;
+          }
+      }
+      if (args.length > 2) {
+          lastError = args[2];
+      } else {
+          lastError = "invalid response";
+      }
+      control.raiseEvent(event_src_esp8266wifi, event_connect_failed);
   }
 
   /**
-   * HTTPS Get
+   * HTTP Get
    * @param url URL for internet resource ,eg: "http://host-address/path..."
    * @param fingerprint SHA1 fingerprint for server certificate (need for https://) ,eg: ""
    */
   //% blockId=esp8266wifi_https_get block="http get url %url |fingerprint %fingerprint"
   //% blockExternalInputs = 1
   export function httpGet(url: string, fingerprint: string): void {
-    send("g," + url + "," + fingerprint);
-    let args = parse(recv());
-    if (args.length > 3) {
-      lastResult = args[3];
-      if (args[1] === "200") {
-        control.raiseEvent(event_src_esp8266wifi, event_response_succeeded);
-        return;
+      send("g," + url + "," + fingerprint);
+      let args = parse(recv(8192, 10000));
+      if (args.length > 3) {
+          lastResult = args[3];
+          if (args[1] === "200") {
+              control.raiseEvent(event_src_esp8266wifi, event_response_succeeded);
+              return;
+          }
       }
-    }
-    if (args.length > 2) {
-      lastError = args[2];
-    } else {
-      lastError = "invalid response";
-    }
-    control.raiseEvent(event_src_esp8266wifi, event_response_failed);
+      if (args.length > 2) {
+          lastError = args[2];
+      } else {
+          lastError = "invalid response";
+      }
+      control.raiseEvent(event_src_esp8266wifi, event_response_failed);
   }
 
   /**
@@ -211,24 +214,24 @@ namespace esp8266wifi {
   //% blockId=esp8266wifi_https_post block="http post url %url |fingerprint %fingerprint |data %data"
   //% blockExternalInputs = 1
   export function httpPost(
-    url: string,
-    fingerprint: string,
-    data: string = ""
+      url: string,
+      fingerprint: string,
+      data: string = ""
   ): void {
-    send("p," + url + "," + fingerprint + "," + data);
-    let args = parse(recv());
-    if (args.length > 3) {
-      lastResult = args[3];
-      if (args[1] === "200") {
-        control.raiseEvent(event_src_esp8266wifi, event_response_succeeded);
-        return;
+      send("p," + url + "," + fingerprint + "," + data);
+      let args = parse(recv(8192, 10000));
+      if (args.length > 3) {
+          lastResult = args[3];
+          if (args[1] === "200") {
+              control.raiseEvent(event_src_esp8266wifi, event_response_succeeded);
+              return;
+          }
       }
-    }
-    if (args.length > 2) {
-      lastError = args[2];
-    } else {
-      lastError = "invalid response";
-    }
-    control.raiseEvent(event_src_esp8266wifi, event_response_failed);
+      if (args.length > 2) {
+          lastError = args[2];
+      } else {
+          lastError = "invalid response";
+      }
+      control.raiseEvent(event_src_esp8266wifi, event_response_failed);
   }
 }
